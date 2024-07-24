56793fa1192b7523e51c312647bfc490d36d374d
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-24 22:16:48 +0100

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,30 +1,38 @@
-#!/usr/bin/python3
-""" 3-main """
-LRUCache = __import__('3-lru_cache').LRUCache
+#!/usr/bin/env python3
+""" LRU caching
+"""
 
-my_cache = LRUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache
+    """
+    def __init__(self):
+        super().__init__()
+        self.history = []
+
+    def put(self, key, item):
+        """ assigns the new item to the dictionary
+        """
+        if not (key is None or item is None):
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                 and key not in self.cache_data:
+                print(f'DISCARD: {self.history[0]}')
+                self.cache_data.pop(self.history[0])
+                del self.history[0]
+            if key in self.history:
+                del self.history[self.history.index(key)]
+            self.history.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ returns the value in self.cache_data linked to key
+        """
+        if key is None or not (key in self.cache_data):
+            return None
+        else:
+            del self.history[self.history.index(key)]
+            self.history.append(key)
+            return self.cache_data[key]
