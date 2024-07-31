0ee67704168c7f913264423be690216c3dd94bec
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-07-31 03:46:14 +0100

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -38,7 +38,7 @@ def index():
     """GET /
     Return: 4-index.html
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
